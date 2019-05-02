18fb8985ca7b7d11d1a469fb52562e3e0ea26d94
ArifulProtik <mdarifulislam.protik@gmail.com>
2019-05-02 14:36:07 +0600

    adding Commit

---
+++ rev.py
@@ -0,0 +1,5 @@
+# https://viewdns.info/reverseip/?host=github.com&t=1 
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
