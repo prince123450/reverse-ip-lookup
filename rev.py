a53ff14d5f848619a5d1eeafdad97ca6f28c518c
ArifulProtik <mdarifulislam.protik@gmail.com>
2019-05-02 19:12:29 +0600

    Adding Skeleton

--- rev.py
+++ rev.py
@@ -1,5 +1,22 @@
-# https://viewdns.info/reverseip/?host=github.com&t=1 
 import requests
 from bs4 import BeautifulSoup
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+file = open('list.txt', 'r').read().split('\n')
+for link in file:
+        if link == "":
+                continue
+        url = "https://viewdns.info/reverseip/?host="+link+"&t=1"
+        text = requests.get(url, headers=headers).text
+        soup = BeautifulSoup(text, 'html.parser')
+        narrow = soup.find('table', attrs={'border':'1'})
+        domain = narrow.findAll('td', attrs={'align':None})
+        for line in domain:
+                print(line.text)
+
+
+        
+    
+
+
+    
